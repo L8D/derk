e8571f7e0fc58fb0a845328ad2a2a5cd351b51bd
L8D <tenorbiel@gmail.com>
2013-07-18 23:29:38 -0500

    added initial scriptw

---
+++ derk.rb
@@ -0,0 +1,15 @@
+#/usr/bin/env ruby
+require 'irc/base'
+
+class Derk < IRC::Bot
+  host    'irc.unixhub.net'
+  nick    'derk'
+  port    6667
+  channel '#unixhub'
+
+  mention_match /reload!/ do
+    self.class.reset!
+    load __FILE__
+  end
+  start!
+end
